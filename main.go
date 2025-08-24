c0c8a97ec71bc976e934586118ea7403182fe140
Aditya Mandal <mandaladitya1614@gmail.com>
2025-08-24 20:56:09 +0530

    Built basic rssagg

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Aditya-1304/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -37,14 +38,14 @@ func main()  {
 		log.Fatal("Can't connect to database:", err)
 	}
 
-	// queries, err := database.New(conn)
-	// if err != nil {
-	// 	log.Fatal("Can't create to bd connection:", err)
-	// }
+	db := database.New(conn)
 	apiCfg := apiConfig {
-		DB: database.New(conn),
+		DB: db,
 	} 
 
+	go startScraping(
+		db, 10, time.Minute,
+	)
 
 	router := chi.NewRouter()
 
@@ -66,8 +67,11 @@ func main()  {
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostForUser))
+
 	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
 	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
+	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow))
 
 
 	router.Mount("/v1", v1Router)
